2d70c1bf49cf58fda71f6e297d408aaa382905d0
ashishkamapni8 <ashishkampani8@gmail.com>
2024-02-14 16:52:20 +0530

    initial commit

---
+++ deliveryboy/src/main/java/com/deliveryboy/deliveryboy/config/KafkaConfig.java
@@ -0,0 +1,18 @@
+package com.deliveryboy.deliveryboy.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.internals.Topic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public NewTopic Topic(){
+
+        return TopicBuilder.name(AppConstants.LOCATION_TOPIC_NAME)
+                .build();
+    }
+}
